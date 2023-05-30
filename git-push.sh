18bb7eba5e118131681e41d06be0c8f898b6fb02
yaunsine <1580909148@qq.com>
2023-05-30 18:15:10 +0800

    first commit 0.0.1

--- git-push.sh
+++ git-push.sh
@@ -1,7 +1,5 @@
-echo "# docker-my-blog" >> README.md
 git init
 git add .
 git commit -m "first commit 0.0.1"
 git branch -M master
-git remote add origin https://github.com/yaunsine/docker-my-blog.git
 git push -u origin master
